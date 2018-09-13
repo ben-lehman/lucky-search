28b9f83dcb5cb88236d8e6ae45508fa5d7318327
Ben Lehman <ben.a.lehman@gmail.com>
2018-09-13 15:51:36 -0400

    Updated Google search url

--- lucky.py
+++ lucky.py
@@ -4,7 +4,7 @@
 import requests, sys, webbrowser, bs4
 
 print('Googling...') #display while downloading pages
-res = requests.get('http://google.com/search?1=' + ' '.join(sys.argv[1:]))
+res = requests.get('https://www.google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 # Retrieve top search result links
@@ -16,5 +16,4 @@ linkElems = soup.select('.r a')
 
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
-	print("Opening");
 	webbrowser.open('http://google.com' + linkElems[i].get('href'))
